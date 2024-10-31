afe293005bba5a1ea28f4a27974913a9d232948e
Patryk Skwara <94368703+SkwarekP@users.noreply.github.com>
2024-10-31 22:56:17 +0100

    31-10-2024 (#5)

--- src/exam/exam.service.ts
+++ src/exam/exam.service.ts
@@ -11,7 +11,7 @@ export class ExamService {
   ) {}
 
   async getAllExams(): Promise<Exam[]> {
-    return this.examRepository.find({ relations: ['questions'] });
+    return this.examRepository.find({ relations: ['questions', 'executions'] });
   }
 
   async getExam(examId: number): Promise<Exam> {
