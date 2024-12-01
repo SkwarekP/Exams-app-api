75a9307e6106237c953bfaa9d850aa896bde51f3
SkwarekP <patryk.skwara@proton.me>
2024-12-01 14:44:05 +0100

    01-12-2024 remove answers

--- src/answers/answers.service.ts
+++ src/answers/answers.service.ts
@@ -1,101 +1,101 @@
-import { BadRequestException, forwardRef, Inject, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Answers } from './entity/answers.entity';
-import { Repository } from 'typeorm';
-import { CorrectAnswers } from './answers.types';
-import { ExecutionsService } from 'src/executions/executions.service';
-import { ExamService } from 'src/exam/exam.service';
-import { AnswersCreationAttributes, CreateAnswersDto } from './Dto/create-answers.dto';
+// import { BadRequestException, forwardRef, Inject, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Answers } from './entity/answers.entity';
+// import { Repository } from 'typeorm';
+// import { CorrectAnswers } from './answers.types';
+// import { ExecutionsService } from 'src/executions/executions.service';
+// import { ExamService } from 'src/exam/exam.service';
+// import { AnswersCreationAttributes, CreateAnswersDto } from './Dto/create-answers.dto';
 
-@Injectable()
-export class AnswersService {
-  addAnswer(examId: string) {
-    throw new Error('Method not implemented.');
-  }
-  constructor(
-    @InjectRepository(Answers)
-    private answersRepository: Repository<Answers>,
-    @Inject(forwardRef(() => ExecutionsService)) 
-    private executionService: ExecutionsService,
-    @Inject(forwardRef(() => ExamService))
-    private examService: ExamService
-  ) {}
+// @Injectable()
+// export class AnswersService {
+//   addAnswer(examId: string) {
+//     throw new Error('Method not implemented.');
+//   }
+//   constructor(
+//     @InjectRepository(Answers)
+//     private answersRepository: Repository<Answers>,
+//     @Inject(forwardRef(() => ExecutionsService)) 
+//     private executionService: ExecutionsService,
+//     @Inject(forwardRef(() => ExamService))
+//     private examService: ExamService
+//   ) {}
 
-  async getAllAnswers(): Promise<Answers[]> {
-    return this.answersRepository.find();
-  }
+//   async getAllAnswers(): Promise<Answers[]> {
+//     return this.answersRepository.find();
+//   }
 
-  async getAnswersByExecutionId(
-    executionId: string,
-  ): Promise<CorrectAnswers[]> {
+//   async getAnswersByExecutionId(
+//     executionId: string,
+//   ): Promise<CorrectAnswers[]> {
 
-    try {
-      const execution = await this.executionService.findExecution(executionId)
+//     try {
+//       const execution = await this.executionService.findExecution(executionId)
 
-      if(!execution) {
-        throw new NotFoundException("Execution not found")
-      }
+//       if(!execution) {
+//         throw new NotFoundException("Execution not found")
+//       }
 
-      const answers = await this.answersRepository.find({
-        where: {examId: execution.examId}
-      })
+//       const answers = await this.answersRepository.find({
+//         where: {examId: execution.examId}
+//       })
 
-      const mappedAnswers = answers.map((answer) => {
-        return {answerId: answer.answerId, correctAnswer: answer.correctAnswer}
-      })
+//       const mappedAnswers = answers.map((answer) => {
+//         return {answerId: answer.answerId, correctAnswer: answer.correctAnswer}
+//       })
 
-      return mappedAnswers;
-    } catch (error) {
-      throw new InternalServerErrorException("There is an error in Answers Service: ", error.message)
-    }
-  }
+//       return mappedAnswers;
+//     } catch (error) {
+//       throw new InternalServerErrorException("There is an error in Answers Service: ", error.message)
+//     }
+//   }
 
-  async getAnswersByExamId(
-    examId: string,
-  ): Promise<CorrectAnswers[]> {
-    try {
-      const exam = await this.examService.getExam(examId)
+//   async getAnswersByExamId(
+//     examId: string,
+//   ): Promise<CorrectAnswers[]> {
+//     try {
+//       const exam = await this.examService.getExam(examId)
 
-      if(!exam) {
-        throw new NotFoundException("Exam not found while getting answers by exam id")
-      }
+//       if(!exam) {
+//         throw new NotFoundException("Exam not found while getting answers by exam id")
+//       }
 
-      const answers = await this.answersRepository.find({
-        where: {examId: exam.examId}
-      })
+//       const answers = await this.answersRepository.find({
+//         where: {examId: exam.examId}
+//       })
 
-      const mappedAnswers = answers.map((answer) => {
-        return {answerId: answer.answerId, correctAnswer: answer.correctAnswer}
-      })
+//       const mappedAnswers = answers.map((answer) => {
+//         return {answerId: answer.answerId, correctAnswer: answer.correctAnswer}
+//       })
 
-      return mappedAnswers;
-    } catch (error) {
-      throw new InternalServerErrorException("There is an error in Answers Service: ", error.message)
-    }
-  }
+//       return mappedAnswers;
+//     } catch (error) {
+//       throw new InternalServerErrorException("There is an error in Answers Service: ", error.message)
+//     }
+//   }
 
-  async getCorrectAnswer(questionId: string): Promise<string> {
-    const correctAnswer = await this.answersRepository.findOne({
-      where: { answerId: questionId },
-    });
-    if (!correctAnswer) {
-      throw new NotFoundException(
-        'There is no correct answer in database',
-      );
-    }
+//   async getCorrectAnswer(questionId: string): Promise<string> {
+//     const correctAnswer = await this.answersRepository.findOne({
+//       where: { answerId: questionId },
+//     });
+//     if (!correctAnswer) {
+//       throw new NotFoundException(
+//         'There is no correct answer in database',
+//       );
+//     }
 
-    return correctAnswer.correctAnswer;
-  }
+//     return correctAnswer.correctAnswer;
+//   }
 
-  async addAnswerToDatabase(createAnswersDto: CreateAnswersDto[]): Promise<Answers[]> {
-    try {
-      const createAnswers = this.answersRepository.create(createAnswersDto);
+//   async addAnswerToDatabase(createAnswersDto: CreateAnswersDto[]): Promise<Answers[]> {
+//     try {
+//       const createAnswers = this.answersRepository.create(createAnswersDto);
 
-      return await this.answersRepository.save(createAnswers);
+//       return await this.answersRepository.save(createAnswers);
 
-    } catch (error) {
-        throw new BadRequestException(`Failed to insert answers: ${error.message}`)
-    }
-  }
+//     } catch (error) {
+//         throw new BadRequestException(`Failed to insert answers: ${error.message}`)
+//     }
+//   }
 
-}
+// }
--- src/exam/exam.module.ts
+++ src/exam/exam.module.ts
@@ -4,7 +4,6 @@ import { ExamService } from './exam.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Exam } from './entity/exam.entity';
 import { ExecutionsModule } from 'src/executions/executions.module';
-import { AnswersModule } from 'src/answers/answers.module';
 import { QuestionsModule } from 'src/questions/questions.module';
 import { UsersModule } from 'src/users/users.module';
 
@@ -13,8 +12,7 @@ import { UsersModule } from 'src/users/users.module';
     TypeOrmModule.forFeature([Exam]),
     QuestionsModule,
     UsersModule,
-    forwardRef(() => ExecutionsModule),
-    forwardRef(() => AnswersModule)
+    forwardRef(() => ExecutionsModule)
   ],
   controllers: [ExamController],
   providers: [ExamService],
